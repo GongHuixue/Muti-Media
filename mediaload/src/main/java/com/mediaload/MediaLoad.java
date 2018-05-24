fb2b924afcc55184b79f453e300cb43ea707cabf
huixue.gong <huixue.gong@tpv-tech.com>
2018-05-24 20:51:57 +0800

    add one load task

--- mediaload/src/main/java/com/mediaload/MediaLoad.java
+++ mediaload/src/main/java/com/mediaload/MediaLoad.java
@@ -25,7 +25,7 @@ import java.util.Queue;
 
 public class MediaLoad {
     private final String TAG = MediaLoad.class.getSimpleName();
-    private Map<String, Queue<LoadTask>> mTaskGroup = new HashMap<>();
+    private Map<String, LoadTask> mTask = new HashMap<>();
     private Map<String, Integer> mLoadId = new HashMap<>();
 
     private static MediaLoad mediaLoadInstance;
@@ -41,26 +41,38 @@ public class MediaLoad {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
+            String activityName;
+            LoadTask loadTask;
             Log.d(TAG, "Handle Msg = " + msg.what + ", FragmentActivity = " + (String)msg.obj);
             switch (msg.what) {
                 case MSG_LOAD_START:
-                    String activityName = (String)msg.obj;
+                    activityName = (String)msg.obj;
                     if(!TextUtils.isEmpty(activityName)) {
-                        Queue<LoadTask> queue = mTaskGroup.get(activityName);
-                        LoadTask task = queue.poll();
-                        if(task != null) {
-                            queueLoad(task.activityWeakReference.get(), task.onLoadCallBack);
+                        loadTask = mTask.get(activityName);
+                        if(loadTask != null) {
+                            queueLoad(loadTask.activityWeakReference.get(), loadTask.onLoadCallBack);
                         }
                     }
                     break;
                 case MSG_LOAD_STOP:
-                    break;
-                case MSG_LOAD_ERROR:
+                    activityName = (String)msg.obj;
+                    if(!TextUtils.isEmpty(activityName)) {
+                        loadTask = mTask.get(activityName);
+                        int loadId = mLoadId.get(activityName);
+
+                        if((loadTask != null) && (loadTask.activityWeakReference.get() != null)){
+                            loadTask.activityWeakReference.get().getSupportLoaderManager().destroyLoader(loadId);
+                        }
+                        mTask.clear();
+                        mLoadId.clear();
+                    }
                     break;
                 case MSG_LOAD_FINISH:
+                    mTask.clear();
+                    mLoadId.clear();
                     break;
                 default:
-                        break;
+                    break;
             }
         }
     };
@@ -105,16 +117,12 @@ public class MediaLoad {
 
     private synchronized void startLoad(FragmentActivity activity, OnLoadCallBack loadCallBack) {
         String activityName = activity.getClass().getSimpleName();
-        Queue<LoadTask> queue = mTaskGroup.get(activityName);
         LoadTask task = new LoadTask(new WeakReference<FragmentActivity>(activity), loadCallBack);
 
-        if(queue == null) {
-            queue = new LinkedList<>();
-            mTaskGroup.put(activityName, queue);
-        }
+        if(!mTask.containsKey(activityName)) {
+            mTask.put(activityName, task);
 
-        queue.offer(task);
-        if(queue.size() == 1) {
+            /*send start load message*/
             Message message = Message.obtain();
             message.what = MSG_LOAD_START;
             message.obj = activityName;
@@ -122,7 +130,7 @@ public class MediaLoad {
         }
     }
 
-    public void stopLoad(FragmentActivity activity, OnLoadCallBack loadCallBack) {
+    public void stopLoad(FragmentActivity activity) {
 
     }
 
@@ -131,19 +139,19 @@ public class MediaLoad {
             @Override
             public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
                 super.onLoadFinished(loader, data);
-                Queue<LoadTask> queue = mTaskGroup.get(activity.getClass().getSimpleName());
-                if(queue != null) {
-                    queue.clear();
-                }
+                Message message = Message.obtain();
+                message.what = MSG_LOAD_FINISH;
+                message.obj = activity.getClass().getSimpleName();
+                mHandler.sendMessage(message);
             }
 
             @Override
             public void onLoaderReset(Loader<Cursor> loader) {
                 super.onLoaderReset(loader);
-                Message message = Message.obtain();
-                message.what = MSG_LOAD_FINISH;
-                message.obj = activity.getClass().getSimpleName();
-                mHandler.sendMessage(message);
+                LoadTask task = mTask.get(activity.getClass().getSimpleName());
+                if(task != null) {
+                    mTask.clear();
+                }
             }
         });
     }
