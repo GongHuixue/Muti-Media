ce036510c2b6a0269b5c473c00ef400b5e4488be
huixue.gong <huixue.gong@tpv-tech.com>
2018-07-24 09:07:19 +0800

    add photo view adapter

---
+++ app/src/main/java/com/example/android/multmedia/player/photo/PhotoViewAdapter.java
@@ -0,0 +1,10 @@
+package com.example.android.multmedia.player.photo;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+
+public class PhotoViewAdapter extends PagerAdapter {
+    private Context context;
+
+
+}
